eae11d3c29f90756d45de20b1e92e46a0044bb8a
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2023-01-06 16:03:47 +0700

    SS2

---
+++ SS-2/coding-challenge2.js
@@ -0,0 +1,16 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn*heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn
+// console.log("BMIJohn :" +BMIJohn )
+// console.log("BMIMark :" +BMIMark)
+if(BMIMark > BMIJohn){
+        console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+}else{
+        console.log(`John's BMI (${BMIJohn}) is higher than Mark's ${BMIMark}!`)
+}
+// console.log(markHigherBMI)
